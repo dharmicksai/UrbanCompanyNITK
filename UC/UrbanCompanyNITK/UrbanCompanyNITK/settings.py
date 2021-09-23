3e20a47c21dea298743df579cf8419e7c6b0d9d5
Prajwaljm <prajwaljm.7s@gmail.com>
2021-09-23 15:49:32 +0530

    First Draft of Customer module

--- UC/UrbanCompanyNITK/UrbanCompanyNITK/settings.py
+++ UC/UrbanCompanyNITK/UrbanCompanyNITK/settings.py
@@ -125,3 +125,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'UrbanCompanyNITK/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- UC/UrbanCompanyNITK/ucnitk/views.py
+++ UC/UrbanCompanyNITK/ucnitk/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'ucnitk/index.html')
+    return render(request, 'ucnitk/customer.html')
 
 def add_something(request):
     return render(request, 'ucnitk/add_something.html')
