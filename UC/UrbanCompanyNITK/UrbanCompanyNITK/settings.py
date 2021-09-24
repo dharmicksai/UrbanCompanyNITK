18a139c847e925d1bf8027ba483e65d3d04a46a9
sanguvk123 <sanguvk123@gmail.com>
2021-09-24 14:56:48 +0530

    email validation

--- UC/UrbanCompanyNITK/UrbanCompanyNITK/settings.py
+++ UC/UrbanCompanyNITK/UrbanCompanyNITK/settings.py
@@ -133,5 +133,14 @@ MEDIA_URL = '/media/'
 
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
-
 }
+
+
+
+#email stuff
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
--- UC/UrbanCompanyNITK/authentication/views.py
+++ UC/UrbanCompanyNITK/authentication/views.py
@@ -5,6 +5,8 @@ from django.http import JsonResponse
 from django.contrib.auth.models import User
 # from validate_email import validate_email
 from django.contrib import messages
+from django.core.mail import EmailMessage
+
 class RegistrationView(View):
     def get(self, request):
         return render(request, 'authentication/register.html')
@@ -28,7 +30,17 @@ class RegistrationView(View):
 
                 user = User.objects.create_user(username = username, email = email)
                 user.set_password(password)
+                user.is_active = False
                 user.save()
+                email_subject = 'Activate your account'
+                email_body = ' test body'
+                email = EmailMessage(
+                    email_subject,
+                    email_body,
+                    'noreply@semycolon.com',
+                    [email],
+                )
+                email.send(fail_silently=False)
                 messages.success(request, 'Account created successfully')
                 return render(request, 'authentication/register.html')
 
