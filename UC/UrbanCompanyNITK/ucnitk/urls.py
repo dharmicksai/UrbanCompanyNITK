ad664c0b70066df1c58a765b1a894f8c6aca181a
Prajwaljm <prajwaljm.7s@gmail.com>
2021-10-03 00:35:44 +0530

    new template for order-service form

--- UC/UrbanCompanyNITK/ucnitk/urls.py
+++ UC/UrbanCompanyNITK/ucnitk/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.index, name = "ucnitk"),
     path('add-something', views.add_something, name = "add-something"),
     path('order-service', views.order_service, name = "order-service"),
-    path('service_provider', service_provider.as_view(), name = "service-provider")
+    path('service-provider', service_provider.as_view(), name = "service-provider")
 ]
