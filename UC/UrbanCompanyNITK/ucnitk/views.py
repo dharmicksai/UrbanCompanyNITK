8b0734aaa6f4cbb8341f23236330fb0faf9abbd4
dharmicksai <dharmicksaik@gmail.com>
2021-10-04 16:03:39 +0530

    Added your orders and accepted orders page

--- UC/UrbanCompanyNITK/ucnitk/views.py
+++ UC/UrbanCompanyNITK/ucnitk/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic import DetailView
 from .models import *
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -32,10 +33,29 @@ def order_service(request):
 
 class service_provider(View):
     def get(self, request):
+        user = request.user
         context = {
-            'orders': Order.objects.all()
+            'orders': Order.objects.exclude(Customer = user).filter(ServiceProvider = None)
         }
         return render(request, 'ucnitk/service_provider.html', context)
+
+# @login_required(login_url='/login/')
+class your_orders(View):
+    def get(self, request):
+        user = request.user
+        context = {
+            'orders': Order.objects.filter(Customer = user)
+        }
+        return render(request, 'ucnitk/your_orders.html', context)
+
+# @login_required(login_url='/login/')
+class accepted_orders(View):
+    def get(self, request):
+        user = request.user
+        context = {
+            'orders': Order.objects.filter(ServiceProvider = user)
+        }
+        return render(request, 'ucnitk/accepted_orders.html', context)
 
 class OrderDetailView(DetailView):
     model = Order
