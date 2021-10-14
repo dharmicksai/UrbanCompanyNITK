f6dc2f10fa67f441b85a7753b868410f2a6fac08
Prajwaljm <prajwaljm.7s@gmail.com>
2021-10-14 12:05:40 +0530

    Added checkout payment option on frontend

--- UC/UrbanCompanyNITK/UrbanCompanyNITK/settings.py
+++ UC/UrbanCompanyNITK/UrbanCompanyNITK/settings.py
@@ -142,9 +142,23 @@ MESSAGE_TAGS = {
 
 
 #email stuff
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
+'''EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 EMAIL_PORT = 587
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')'''
+
+#Comment this part and uncomment above part if needed
+DB_NAME='UCDB'
+DB_USER='root'
+DB_USER_PASSWORD='password'
+DB_HOST='localhost'
+EMAIL_HOST_PASSWORD='UrbanClap123'
+EMAIL_HOST_USER='urbanclapnitk@gmail.com'
+DEFAULT_FROM_EMAIL='urbanclapnitk@gmail.com'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+
+#Payments stuff
